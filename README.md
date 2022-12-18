22909a99696d1d728c1e0a88dd38cde7d6e3924d
Frank Lemmen <frank@familie-lemmen.nl>
2022-12-18 20:52:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Buttonbox software for Assetto Corsa Competizione
 Files and Macros for usage for Assetto Corsa Competizione
 Works perfectly for Xbox X
 
+Simply mount the RP2040 in read/write mode and copy these files to the board.
+
+2x macros
+- 1x for generic controls
+- 1x navigation in car HUD
+
+Switch to other marco via rotator knob
+
+Press switch and it will enter "sleep" mode
+
+Tested on circuitpytion 7.3.3
---
+++ macros/Assetto2.py
@@ -0,0 +1,27 @@
+# MACROPAD Hotkeys: Blender
+
+from adafruit_hid.keycode import Keycode
+
+app = {
+    'name' : 'Display',
+    'order': 2,
+    'macros' : [
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x000000, '', [Keycode.KEYPAD_SEVEN]),
+        (0x663F00, 'UP', [Keycode.KEYPAD_EIGHT]),
+        (0x000000, '      ', [Keycode.KEYPAD_NINE]),
+        # 2nd row ----------
+        (0x663F00, 'LEFT', [Keycode.KEYPAD_FOUR]),
+        (0x360116, 'MFD ', [Keycode.INSERT]),
+        (0x663F00, 'RGHT', [Keycode.KEYPAD_SIX]),
+        # 3rd row ----------
+        (0x000000, ' ', [Keycode.KEYPAD_ONE]),
+        (0x663F00, 'DOWN', [Keycode.KEYPAD_TWO]),
+        (0x000000, '  ', [Keycode.KEYPAD_THREE]),
+        # 4th row ----------
+        (0x0A3B66, 'CAM', [Keycode.F1]),
+        (0x000000, '', [Keycode.INSERT]),
+        (0x404040, 'EXTVIEW', [Keycode.F3])
+    ]
+}

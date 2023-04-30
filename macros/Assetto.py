2afa995cf2f3d8e7dd5be424b9e4480659f99be3
Frank Lemmen <frank@familie-lemmen.nl>
2023-04-30 20:04:46 +0200

    Add files via upload

---
+++ macros/Assetto.py
@@ -0,0 +1,30 @@
+# MACROPAD Hotkeys: Assetto Corsa Competizone
+# Definition by: Frank Lemmen
+
+from adafruit_hid.keycode import Keycode
+# from adafruit_hid.consumer_control_code import ConsumerControlCode
+# from consumer import Toolbar
+
+app = {
+    'name' : 'Assetto Corsa Comp',
+    'order': 1,
+    'macros' : [
+ 	# COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x003000, 'TC',   [Keycode.CONTROL, Keycode.T]),
+        (0x000010, 'BB+',  [Keycode.SHIFT, Keycode.B]),
+        (0x101000, 'ABS', [Keycode.SHIFT, Keycode.A]),
+        # 2nd row ----------
+        (0x442E00, 'ENG', [Keycode.SHIFT, Keycode.E]),
+        (0x000010, 'BB-', [Keycode.CONTROL, Keycode.B]),
+        (0x400000, 'STRT',[Keycode.S]),
+        # 3rd row ----------
+        (0x250445, 'RCL',  [Keycode.ALT, Keycode.D]),
+        (0x250115, 'HUD',  [Keycode.INSERT]),
+        (0x166000, 'OFF', [Keycode.SHIFT, Keycode.I]),
+        # 4th row ----------L
+        (0x0A3B44, 'FLS', [Keycode.SHIFT, Keycode.L]),
+        (0x404040, 'RLT', [Keycode.CONTROL, Keycode.L]),
+        (0x101010, 'LGT', [Keycode.L])
+    ]
+}
